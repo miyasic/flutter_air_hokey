f757629daad6f53127171dabb134df6d639496f4
miyaji555 <kou.sepak@gmail.com>
2023-12-07 06:55:32 +0900

    不要な定数を削除

--- packages/client/lib/constants/constants.dart
+++ packages/client/lib/constants/constants.dart
@@ -13,12 +13,8 @@ const Color kPaddleColor = Colors.blue;
 //Ball Settings
 const double kBallRadius = 10;
 const double kBallSpeed = 500;
-const double kBallStartYRatio = 1 / 2;
 const double kBallMinSpawnAngle = 45;
 const double kBallMaxSpawnAngle = 135;
-const int kBallRandomNumber = 5;
-const double kBallNudgeSpeed = 300;
-const double kBallUncontrolledPositionY = 10;
 const Color kBallColor = Colors.white;
 
 // Math Settings
@@ -28,7 +24,6 @@ const double kRad = pi / 180;
 const double kButtonWidth = 200;
 const double kButtonHeight = 50;
 const Color kButtonColor = Colors.blue;
-const Color kGameOverButtonColor = Colors.red;
 const Color kDisabledColor = Colors.black38;
 
 //Countdown Settings
@@ -38,29 +33,10 @@ const TextStyle kCountdownTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
   fontSize: 160,
 );
+
 const int kCountdownDuration = 3;
 
-//Game Settings
-const int kGameTryCount = 3;
-const int kBlocksColumnCount = 2;
-const int kBlocksRowCount = 3;
-
-//Block Settings
-const double kBlocksStartYPosition = 50;
-const double kBlocksStartXPosition = 50;
-const double kBlocksHeightRatio = 1 / 3;
-const List<MaterialColor> kBlockColors = [
-  Colors.red,
-  Colors.blue,
-  Colors.green,
-  Colors.yellow,
-  Colors.purple,
-  Colors.orange,
-];
-const String kBlockPositionX = 'x';
-const String kBlockPositionY = 'y';
-const double kBlockPadding = 5;
-
+// Wall Settings
 const double wallThickness = 10;
 const PaletteEntry wallColor = BasicPalette.lightOrange;
 
