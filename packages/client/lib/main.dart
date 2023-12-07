3f2aec9fd4b09cc56dc7f70bbb521f69a6b23e40
miyaji555 <kou.sepak@gmail.com>
2023-12-07 09:19:28 +0900

    overlay widgetを削除

--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:air_hokey_client/game/air_hokey.dart';
-import 'package:air_hokey_client/overlay.dart';
 
 void main() {
   final game = AirHokey();
@@ -9,13 +8,8 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SafeArea(
-        child: Stack(
-          children: [
-            GameWidget(
-              game: game,
-            ),
-            const OverlayWidget(),
-          ],
+        child: GameWidget(
+          game: game,
         ),
       ),
     ),
