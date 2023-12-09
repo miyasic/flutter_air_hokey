02abddb4731f67387eb635b1fe85036f356b19a9
miyaji555 <kou.sepak@gmail.com>
2023-12-09 11:36:06 +0900

    ws.dartの不要なコメントを削除

--- packages/server/routes/ws.dart
+++ packages/server/routes/ws.dart
@@ -13,16 +13,11 @@ import 'package:uuid/v4.dart';
 Future<Response> onRequest(RequestContext context) async {
   final handler = webSocketHandler(
     (channel, protocol) {
-      // A new client has connected to our server.
-      // Subscribe the new client to receive notifications
-      // whenever the cubit state changes.
       final cubit = context.read<GameCubit>()..subscribe(channel);
-
       final uuid = const UuidV4().generate();
       final userRole = cubit.onNewAccess(uuid);
       final handshake =
           Handshake(id: uuid, userRole: userRole, gameState: cubit.state);
-      // Send the current count to the new client.
       final serverResponse = ServerResponse(
         type: ServerResponseType.handshake,
         responseDetail: handshake,
@@ -35,7 +30,6 @@ Future<Response> onRequest(RequestContext context) async {
         ),
       );
 
-      // Listen for messages from the client.
       channel.stream.listen(
         (event) {
           if (event is String) {
@@ -56,8 +50,6 @@ Future<Response> onRequest(RequestContext context) async {
             }
           }
         },
-        // The client has disconnected.
-        // Unsubscribe the channel.
         onDone: () => cubit.unsubscribe(channel),
       );
     },
