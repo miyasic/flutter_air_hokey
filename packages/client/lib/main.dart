e8e17cd467a96e0aa8086d9a979d9e2f01a19e69
miyaji555 <kou.sepak@gmail.com>
2023-12-23 11:53:19 +0900

    Add usePathUrlStrategy

--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:air_hokey_client/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(
     ProviderScope(child: App()),
   );
